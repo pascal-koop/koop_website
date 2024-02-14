b369f3489c164926730947c5935b03c7c0f7205e
Pascal <pascale.koop@live.com>
2024-02-14 21:30:09 +0100

    Refactor navigation links in Navigation.vue

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,5 +1,10 @@
 <script setup>
-
+const links = [
+  { name: 'STORY', to: '#story-section' },
+  { name: 'WORKS', to: '#works-section' },
+  { name: 'EXPERIENCE', to: '#experience-section' },
+  { name: 'CONTACTS', to: '#contacts-section' },
+]
 </script>
 
 <style>
@@ -15,11 +20,12 @@
       </h1>
     </div>
     <div class="nav-links">
-      <ul class="nav-links-list">
-        <li><NuxtLink class="nav-links-item" to="#story-section">STORY</NuxtLink></li>
-        <li><NuxtLink class="nav-links-item" to="#works-section">WORKS</NuxtLink></li>
-        <li><NuxtLink class="nav-links-item" to="#experience-section">EXPERIENCE</NuxtLink></li>
-        <li><NuxtLink class="nav-links-item" to="#contacts-section">CONTACTS</NuxtLink></li>
+      <ul class="nav-link-list">
+        <li v-for="link in links" :key="link.name" class="nav-list-item">
+          <NuxtLink :to="link.to" class="nav-link">
+            {{ link.name }}
+          </NuxtLink>
+        </li>
       </ul>
     </div>
   </nav>
