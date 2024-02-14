49bfca5b026741202c5d9528fabd7fb77616844a
Pascal <pascale.koop@live.com>
2024-02-14 13:59:00 +0100

    Add Navigation component to index page

---
+++ components/Navigation.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<style>
+</style>
+
+<template>
+  <nav class="nav">
+    <div class="nav-logo">
+      <h1 class="nav-title">
+        <NuxtLink class="nav-title-link" to="/">
+          KOOP PASCALE
+        </NuxtLink>
+      </h1>
+    </div>
+    <div class="nav-links">
+      <ul class="nav-links-list">
+        <li><NuxtLink class="nav-links-item" to="#story-section">STORY</NuxtLink></li>
+        <li><NuxtLink class="nav-links-item" to="#works-section">WORKS</NuxtLink></li>
+        <li><NuxtLink class="nav-links-item" to="#experience-section">EXPERIENCE</NuxtLink></li>
+        <li><NuxtLink class="nav-links-item" to="#contacts-section">CONTACTS</NuxtLink></li>
+      </ul>
+    </div>
+  </nav>
+</template>
