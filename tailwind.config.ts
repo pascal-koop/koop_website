cdfde0806bee378324027b55c315b5ce82aa1525
Pascal <79517800+Pascale-Cheddar@users.noreply.github.com>
2024-02-16 19:55:35 +0100

    Update tailwind.css and tailwind.config.ts (#5)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,12 @@
 module.exports = {
-
+  theme: {
+    colors: {
+      bglight: '#FAF0E6',
+      bgdark: '#232121',
+      cardLight: '#EDEBE8',
+      cardDark: '#232121',
+      textLight: '#EDEBE8',
+      textDark: '#232121',
+    }
+  }
 }
