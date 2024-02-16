20ae4d7c27bc586361028a782963dc93acb39cae
Pascal <79517800+Pascale-Cheddar@users.noreply.github.com>
2024-02-16 13:50:30 +0100

    Add hamburger menu (#3)
    * Add mobile navigation styles and functionality
    
    * Refactor navigation menu toggle function

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -5,6 +5,12 @@ const links = [
   { name: 'EXPERIENCE', to: '#experience-section' },
   { name: 'CONTACTS', to: '#contacts-section' },
 ]
+const openMenu = ref(false);
+
+const toggleMenu = () => {
+  openMenu.value = !openMenu.value;
+}
+
 </script>
 
 <style>
@@ -14,15 +20,32 @@ const links = [
   <nav class="nav">
     <div class="nav-logo">
       <h1 class="nav-logo-title">
-        <NuxtLink class="nav-logo-title-link" to="/">
-          KOOP PASCALE
+        <NuxtLink to="/">
+          KOOP
+          PASCALE
         </NuxtLink>
       </h1>
     </div>
-    <div class="nav-links">
-      <ul class="nav-link-list">
-        <li v-for="link in links" :key="link.name" class="nav-list-item">
-          <NuxtLink :to="link.to" class="nav-link">
+    <div class="hamburger" @click="toggleMenu" :class="{ active: openMenu }">
+      <div class="hamburger-line line-1"></div>
+      <div class="hamburger-line line-2"></div>
+      <div class="hamburger-line line-3"></div>
+    </div>
+    <!-- desktop nav -->
+    <div class="desktop-nav-links hidden md:flex">
+      <ul class="desktop-nav-link-list">
+        <li v-for="link in links" :key="link.name" class="desktop-nav-list-item">
+          <NuxtLink :to="link.to" class="desktop-nav-link">
+            {{ link.name }}
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <!-- mobile-nav  -->
+    <div class="mobile-nav" :class="{ active: openMenu}">
+      <ul class="mobile-nav-link-list">
+        <li v-for="link in links" :key="link.name" class="mobile-nav-list-item">
+          <NuxtLink :to="link.to" class="mobile-nav-link">
             {{ link.name }}
           </NuxtLink>
         </li>
