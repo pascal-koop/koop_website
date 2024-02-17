1dbe08f490b4446cd87f65e20029f8cb404cc323
Pascal <79517800+Pascale-Cheddar@users.noreply.github.com>
2024-02-17 15:14:01 +0100

    Add story section, scroll behaviour, an click events (#8)

---
+++ components/Story.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<style></style>
+
+<template>
+	<section id="story-section" class="story">
+		<div class="story-content">
+			<h2 class="story-title title">MY STORY</h2>
+			<p class="story-text">
+				Hi, I'm Pascal, a highly motivated Junior Front-End Developer. <br /><br />
+				My journey to becoming a computer scientist started with a completed apprenticeship as a carpenter and various
+				jobs. But, during the Corona pandemic, I discovered the world of web development, and I realized that I wanted
+				to continue to develop myself further. So, I spent a year learning with great enthusiasm, and the more I delved
+				into it, the more I became passionate about it. That's when I decided to start my training as a computer
+				scientist. <br /><br />
+				I am an open-minded individual who is always eager to learn new things quickly. In my free time, I enjoy
+				reading, Calisthenics (a mixture of strength training and gymnastics), meditation, and house dance. <br /><br />
+				I believe that my ability to continuously face new challenges, as well as my excellent team-oriented and
+				communication skills, make me a valuable addition to any team. I am convinced that my passion for technology and
+				my willingness to constantly learn can contribute to the development of creative solutions and support the team
+				on its path to success.
+			</p>
+		</div>
+	</section>
+</template>
