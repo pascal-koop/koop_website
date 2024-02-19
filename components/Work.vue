072e0b90d4cc3bc06c8569b58bd3da49ffe20a92
Pascal <pascale.koop@live.com>
2024-02-19 11:14:20 +0100

    Update project descriptions and add GitHub links

--- components/Work.vue
+++ components/Work.vue
@@ -1,9 +1,7 @@
 <script setup>
+	import projects from '../assets/projects/projects.json';
 
-import projects  from '../assets/projects/projects.json'
-
-console.log(projects)
-
+	console.log(projects);
 </script>
 
 <style></style>
@@ -12,20 +10,24 @@ console.log(projects)
 	<section
 		id="work-section"
 		class="work">
-		<h1 class="work-card-title title"></h1>
-		<div class="work-card"
-      v-for="project in projects"
-      :key="project.title"
-    >
+		<h1 class="work-card-title title">MY WORK</h1>
+		<div
+			class="work-card"
+			v-for="project in projects"
+			:key="project.title">
 			<img
-        loading="lazy"
+				loading="lazy"
 				:src="project.preview"
-				alt="project-preview"
+				alt="project-preview-image"
 				class="work-card-img" />
-			<h2 class="work-card-subtitle">{{ project.title }}</h2>
-			<h3 class="tech-stack">{{ project.stack }}</h3>
+			<h2 class="work-card-subtitle">{{ project.title.toUpperCase() }}</h2>
+			<h3 class="tech-stack">TECH: {{ project.tech.join(', ').toUpperCase() }}</h3>
 			<p class="work-card-text">{{ project.description }}</p>
-
+			<NuxtLink
+				:to="project.link"
+				class="work-card-link"
+				>GITHUB
+			</NuxtLink>
 		</div>
 	</section>
 </template>
