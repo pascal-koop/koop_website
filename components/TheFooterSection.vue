02d87ac11717c45601947836133cccfba678adcd
Pascal <pascale.koop@live.com>
2024-02-27 15:23:28 +0100

    Add footer section to index.vue

---
+++ components/TheFooterSection.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<style></style>
+
+<template>
+	<footer class="footer">
+		<div class="footer-content mx-5 pb-2">
+			<p class="footer-text font-neueRegrade font-semibold">© 2024 Pascal Koop</p>
+		</div>
+	</footer>
+</template>
