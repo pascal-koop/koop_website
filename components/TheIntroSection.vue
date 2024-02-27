003699a454e0a53e095ab198f2ba547ff977c98b
Pascal <pascale.koop@live.com>
2024-02-27 18:01:38 +0100

    Update introduction text in TheIntroSection.vue

--- components/TheIntroSection.vue
+++ components/TheIntroSection.vue
@@ -7,9 +7,8 @@
 		<button class="introduction-chip font-rota font-normal text-base tracking-[0.2em] border-[1px] pb-2 pt-3 px-6 rounded-full text-textDark">JUNIOR</button>
 		<div class="introduction-content flex flex-col mt-6">
 			<h1 class="title ">FRONT -END DEVELOPER</h1>
-			<p class="introduction-text font-rota mt-28 text-lg leading-relaxed text-textDark"
-				>I'm Pascal, a 32-year-old ambitious junior front-end developer actively seeking new challenges, driven by a
-				passion for learning daily and crafting engaging user interfaces.</p
+			<p class="introduction-text whitespace-pre-line font-rota mt-28 text-lg leading-relaxed text-textDark"
+				>{{ $t('intro') }}</p
 			>
 		</div>
     <div class="introduction-arrow flex flex-col items-center mt-36 animate-bounce z-[1] relative">
