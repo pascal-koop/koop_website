a9b193f3393ddb65cf279b11869de387ed5f1050
Pascal <pascale.koop@live.com>
2024-03-17 14:16:37 +0100

    Refactor story section layout and add image

--- components/TheStorySection.vue
+++ components/TheStorySection.vue
@@ -7,9 +7,17 @@
 		ref="target"
 		id="story-section"
 		class="story mx-5">
-		<div class="story-content">
-			<h2 class="section-title title">{{ $t('sectionTitles.story') }} </h2>
-			<p class="story-text whitespace-pre-line font-rota mt-14 text-lg leading-relaxed text-textDark">
+			<h2 class="section-title title">{{ $t('sectionTitles.story') }}</h2>
+		<div class="story-content md:flex md:flex-row">
+			<NuxtImg
+				class="hidden md:block"
+				src="../public/story-image.png"
+				alt="story image"
+				width="400"
+				height="400"
+				placeholder="empty">
+			</NuxtImg>
+			<p class="story-text whitespace-pre-line font-rota mt-14 text-lg leading-relaxed text-textDark ">
 				{{ $t('story') }}
 			</p>
 		</div>
