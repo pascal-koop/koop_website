a81c3bfd9b99a3a026496356cce29f0890e1eb9b
Pascal <pascale.koop@live.com>
2024-05-13 15:20:11 +0200

    Change animate speed

--- components/TheStorySection.vue
+++ components/TheStorySection.vue
@@ -22,20 +22,20 @@
 		class="story mx-5 mt-24 2xl:mx-0">
 		<h1 class="section-title">{{ $t('sectionTitles.story') }}</h1>
 		<div class="lg:grid lg:grid-flow-col lg:gap-36 mt-14 lg:items-start max-w-md lg:max-w-fit mx-auto">
-			<div ref="story1">
+			<div ref="story1" class="animate__fastest">
 				<h2 class="text-3xl lg:text-4xl font-neueRegrade text-textDark font-normal leading-none md:mt-0">
 					{{ $t('story.firstSegment.title') }}
 				</h2>
 				<p class="text-xl font-rota font-normal mt-4 leading-relaxed">{{ $t('story.firstSegment.content') }}</p>
 			</div>
-			<div ref="story2" class="animate__fast">
+			<div ref="story2" class="animate__faster">
 				<h2
 					class="text-3xl lg:text-4xl font-neueRegrade text-textDark font-normal leading-none mt-6 lg:mt-0"
 					>{{ $t('story.secondSegment.title') }}
 				</h2>
 				<p class="text-xl font-rota font-normal mt-4 leading-relaxed">{{ $t('story.secondSegment.content') }}</p>
 			</div>
-			<div ref="story3" class="animate__faster">
+			<div ref="story3" class="animate__fast">
 				<h2
 					class="text-3xl lg:text-4xl font-neueRegrade text-textDark font-normal leading-none mt-6 lg:mt-0"
 					>{{ $t('story.thirdSegment.title') }}
